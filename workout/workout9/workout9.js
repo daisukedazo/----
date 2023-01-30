852003169d5e15b30c92f424a50ef0712c480e13
nishikawadaisuke <saigou.chan123@gmail.com>
2023-01-30 20:18:31 +0900

    workout9

---
+++ workout/workout9/workout9.js
@@ -0,0 +1,68 @@
+//商品を連想配列でもつ
+var items = [
+    {
+        name: 'ダンベル',
+        price: 5000,
+        quantity:0
+    },
+    {
+        name: 'ベンチ',
+        price: 6500,
+        quantity:0
+    },
+    {
+        name: 'スクワットラック',
+        price: 9000,
+        quantity:0
+    }
+]
+
+var vm = new Vue({
+    el: '#app',
+    date:{
+        items: items
+    }, 
+    filters: {
+        //金額のフォーマット
+        numberWithDelimeter: function(value){
+            if(!value){
+                return '0'
+            }
+            return value.toString().replace(/(\d)(?=(\d{3})+$)/g,'$1')
+        }
+    },
+    methods: {
+        //購入ボタンが押された時の操作
+        doBuy: function(){
+            alert('購入金額は' + this.totalPriceWithTax + '円です')
+            this.items.forEach(function(item){
+                item.quantity = 0
+            })
+        }
+    },
+    computed: {
+        //小計金額
+        totalPrice: function(){
+            return this.items.reduce(function (sum, item){
+                return sum + (item.price * item.quantity)
+            },0)
+        },
+        //税込金額
+        totalPriceWithTax: function(){
+            return Math.floor(this.totalPrice * 1.08)
+        },
+        //購入できる金額以上か判断
+        canBuy: function(){
+            return this.totalPrice >= 20000
+        },
+        //canBuyが偽の時に赤く表示する
+        errorMessageStyle: function(){
+            return {
+                border: this.canBuy ?'':'1px solid red',
+                coler: this.canBuy ?'':'red'
+            }
+        }
+    }
+
+})
+window.vm = vm
